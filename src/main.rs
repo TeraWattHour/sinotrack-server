579aadc7f7904b892b417d6702fe1634aaa07c5d
TeraWattHour <65347730+TeraWattHour@users.noreply.github.com>
2025-04-10 15:44:31 +0200

    feat: sqlx offline

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,7 @@ use std::net::SocketAddr;
 use std::time::Duration;
 use packet::Packet;
 use sqlx::MySql;
-use tokio::io::AsyncWriteExt;
-use tokio::time::{interval, timeout};
+use tokio::time::timeout;
 use tokio::{
     io::{AsyncBufReadExt, BufReader},
     net::{TcpListener, TcpStream},
@@ -58,34 +57,49 @@ async fn handle_connection(stream: TcpStream, pool: Pool<MySql>) -> anyhow::Resu
 
         info!("Message received: {:?}", &message);
         let message = Packet::from_message(&message)?;
+
         match message {
-            Packet::V1 { terminal_no, time, position, speed, direction, battery, .. } => {
-                let obtained_before: Option<(i64, f32, f32)> = sqlx::query_as("select id, lat, lon from locations where device_id = ? and obtained_at < ? order by obtained_at desc limit 1")
-                    .bind(terminal_no)
-                    .bind(time)
+            Packet::V1 { terminal_no, time, position, battery, .. } => {
+                let before = sqlx::query!("select id, ST_Equals(location, ST_GeomFromText(?)) as same_location from readings where device_id = ? and obtained_at < ? order by obtained_at desc limit 1", format!("POINT({} {})", position.1, position.0), terminal_no, time)
                     .fetch_optional(&pool)
                     .await?;
-                if let Some((id, lat, lon)) = obtained_before {
-                    if lat == position.0 && lon == position.1 {
-                        sqlx::query("DELETE FROM locations WHERE id = ?")
-                            .bind(id)
-                            .execute(&pool)
-                            .await?;
+
+                let after = sqlx::query!("select id, ST_Equals(location, ST_GeomFromText(?)) as same_location from readings where device_id = ? and obtained_at > ? order by obtained_at desc limit 1", format!("POINT({} {})", position.1, position.0), terminal_no, time)
+                    .fetch_optional(&pool)
+                    .await?;
+
+                let query = match (before, after) {
+                    (Some(before), Some(after)) if boolean(before.same_location) && boolean(after.same_location) => {
+                        info!("merging three consecutive locations");
+                        // delete the older one, as the duplicates are not needed (same location in contiguous sequence)
+                        Some(sqlx::query!("delete from readings where id = ?", before.id))
+                        // don't insert the new one, as there is a newer record
+                    }
+                    (Some(before), _) if boolean(before.same_location) => {
+                        info!("updating before location");
+                        Some(sqlx::query!("update readings set obtained_at = ?, battery = ? where id = ?", time, battery, before.id))
+                    }
+                    (_, Some(after)) if boolean(after.same_location) => {
+                        info!("already a newer record present, doing nothing");
+                        None
+                    }
+                    _ => {
+                        info!("inserting new record");
+                        Some(sqlx::query!("insert into readings(device_id, obtained_at, location, battery) values (?, ?, ST_GeomFromText(?), ?)", terminal_no, time, format!("POINT({} {})", position.1, position.0), battery))
+                    }
+                };
+
+                if let Some(query) = query {
+                    if let Err(e) = query.execute(&pool).await {
+                        info!("Error executing query: {}", e);
                     }
                 }
-
-                sqlx::query("insert into locations(device_id, obtained_at, lat, lon, speed, direction, battery) values (?, ?, ?, ?, ?, ?, ?)")
-                    .bind(terminal_no)
-                    .bind(time)
-                    .bind(position.0)
-                    .bind(position.1)
-                    .bind(speed)
-                    .bind(direction)
-                    .bind(battery)
-                    .execute(&pool)
-                    .await?;
             }
             Packet::Unknown(_) => info!("Skipping unknown packet"),
         }
     }
+}
+
+fn boolean(v: Option<i32>) -> bool {
+    v.and_then(|v| Some(v != 0)).unwrap_or_default()
 }
